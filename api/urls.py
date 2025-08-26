47c7a4c0ca3e4cacc3b25d598022283f26a339ca
shaikhaseeb-dev <shaikhaseeb004@gmail.com>
2025-08-26 11:56:23 +0530

    Final fixes for deployment

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,13 @@
+# api/urls.py
+
 from django.urls import path
-from .views import register
-from .views import (
-    test_view,
-    public,
-    private,
-    register
-)
+from .views import public, private, register, health_check
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('test/', test_view, name='test'),
     path('public/', public, name='public'),
     path('private/', private, name='private'),
     path('register/', register, name='register'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('health/', health_check, name='health_check'), # The required health check
 ]
